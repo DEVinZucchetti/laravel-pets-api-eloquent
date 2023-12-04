e6518b479a94c4c64b936d69dbc60877414bede8
Verônica Vilas Boas <135287830+VeronicaVilas@users.noreply.github.com>
2023-12-04 11:49:59 -0300

    feat: exercício 06 finalizado

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ClientsController;
 use App\Http\Controllers\PetController;
 use App\Http\Controllers\RaceController;
 use App\Http\Controllers\PeoplesController;
+use App\Http\Controllers\SpecieController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('peoples', PeoplesController::class)->only(
@@ -14,10 +15,14 @@ Route::resource('pets', PetController::class)->only(
   ['index', 'show', 'store', 'update', 'destroy']
 );
 
-
-
 Route::prefix('races')->group(function () {
   Route::get('', [RaceController::class, 'index']);
   Route::post('', [RaceController::class, 'store']);
 });
+
+Route::get('species', [SpecieController::class, 'index']);
+Route::post('species', [SpecieControllerontroller::class, 'store']);
+Route::get('species/{id}', [SpecieControllerontroller::class, 'show']);
+Route::delete('species/{id}', [SpecieControllerontroller::class, 'destroy']);
+Route::put('species/{id}', [SpecieControllerontroller::class, 'update']);
 
